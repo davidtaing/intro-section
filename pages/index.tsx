23a0273669b1712d13adb3726d8c2b0988754df4
David Taing <adavidtaing@gmail.com>
2022-05-09 20:26:42 +1000

    style: header and hero image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -15,9 +16,37 @@ const Home: NextPage = () => {
         <title>Frontend Mentor | Intro section with dropdown navigation</title>
       </Head>
       <main>
-        Features Company Careers About Login Register Make remote work Get your
-        team in sync, no matter your location. Streamline processes, create team
-        rituals, and watch productivity soar. Learn more
+        <header>
+          <a className="logo" href="#">
+            snap
+          </a>
+          <div className="hamburger-menu">
+            <Image
+              src="/images/icon-menu.svg"
+              width="32"
+              height="18"
+              alt="menu"
+              priority
+            />
+          </div>
+        </header>
+        {/* <nav>Features Company Careers About Login Register</nav> */}
+        <div className="hero-image__container">
+          <Image
+            className="hero-image"
+            src="/images/image-hero-mobile.png"
+            width="375"
+            height="282"
+            alt="Hero Image"
+            priority
+          ></Image>
+        </div>
+        <h1>Make remote work</h1>
+        <p>
+          Get your team in sync, no matter your location. Streamline processes,
+          create team rituals, and watch productivity soar.
+        </p>
+        <button>Learn more</button>
         <div className="attribution">
           Challenge by{" "}
           <a
